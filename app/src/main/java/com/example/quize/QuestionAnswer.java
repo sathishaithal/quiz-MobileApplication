f14d6f20389f03690aea90b8305e728e8eb7b073
sathi <sathi111.sssj@gmail.com>
2024-06-14 17:30:11 +0530

    Your commit message

--- app/src/main/java/com/example/quize/QuestionAnswer.java
+++ app/src/main/java/com/example/quize/QuestionAnswer.java
@@ -1,20 +1,20 @@
 package com.example.quize;
 public class QuestionAnswer {
-    public static String question[]={
+    public static String[] question ={
             "Number of primitive data types in Java are?",
             "What is the size of float and double in java?",
             "Automatic type conversion is possible in which of the possible cases?",
             "Arrays in java are-",
             "When an array is passed to a method, what does the method receive?"
     };
-    public static String choices[][]={
+    public static String[][] choices ={
             {"6","7","8","9"},
             {"32 and 64","32 and 32","64 and 32","64 and 64"},
             {"Byte to Int","Int to Long","Long to Int","Short to Int"},
             {"object reference","objects","primitive data type","none"},
             {"The reference of the array","A copy of the array","Length of the array","Copy of first statement"},
     };
-    public static String correctAnswers[]={
+    public static String[] correctAnswers ={
             "8",
             "32 and 64",
             "Int to Long",
